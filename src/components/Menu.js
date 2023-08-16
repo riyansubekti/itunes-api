2d7c46118c59132195548a42c735d3ba5156657d
riyan.subekti <riyan.subekti@mifx.com>
2023-08-16 09:30:34 +0700

    test

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,20 +1,39 @@
-import React, { useContext } from 'react'
-import { HomeContext } from '../context'
-import './Menu.css'
+import { setCookie } from "cookies-next";
+import React, { useContext } from "react";
+import { HomeContext } from "../context";
+import "./Menu.css";
 
 const Menu = () => {
   const { setActiveHome, setActiveSearch } = useContext(HomeContext);
   const handleHome = () => {
     setActiveSearch(true);
-    setActiveHome(prev => !prev);
-  }
+    setActiveHome((prev) => !prev);
+  };
+
+  setCookie("testing", "value-cookies");
+
+  setCookie("testing-2", "value-cookies-2", {
+    domain: ".netlify.app",
+  });
+
+  setCookie("testing-3", "value-cookies-3", {
+    domain: "netlify.app",
+  });
+
+  setCookie("testing-4", "value-cookies-4", {
+    domain: "cheerful-frangipane-2c2fbb.netlify.app",
+  });
 
   return (
     <div className="home--menu">
-      <div onClick={handleHome} className="menu--album">Artist / Album / Title</div>
-      <div onClick={handleHome} className="menu--search">Search</div>
+      <div onClick={handleHome} className="menu--album">
+        Artist / Album / Title
+      </div>
+      <div onClick={handleHome} className="menu--search">
+        Search...
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
